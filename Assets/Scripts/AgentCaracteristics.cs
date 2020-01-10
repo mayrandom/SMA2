fbd3f02374ca45840937ee84d58ec9ac8f8c12f0
egrevet <37064626+egrevet@users.noreply.github.com>
2020-01-10 16:25:38 +0100

    script

--- Assets/Scripts/AgentCaracteristics.cs
+++ Assets/Scripts/AgentCaracteristics.cs
@@ -1,42 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AgentCaracteristics : MonoBehaviour
 {
-    private float memory;
-    private float courage;
-    private float trust;
-    private GameObject[] indices;
+    private bool memory;
+    public bool courage;
+    public bool trust;
+    public List<string> indices = new List<string>();
     public string[] noms;
     public int id = 0;
-    public float[] culpritConfidence;
 
     // Initialize agent
     public void ReceiveAgentParameters(bool Memory, bool Courage, bool Trust, int Id)
     {
+        memory = Memory;
+        courage = Courage;
+        trust = Trust;
         id = Id;
-
-        if (Memory)
-            memory = 0.8f;
-        else
-            memory = 0.2f;
-
-        if (Courage)
-            courage = 0.8f;
-        else
-            courage = 0.2f;
-
-        if (Trust)
-            trust = 0.8f;
-        else
-            trust = 0.2f;
-    }
-
-    void Update()
-    {
-        //après avoir mis à jour les indices, on change le % de confiance envers le coupable
-        //check les taux de confiance
+        indices.Add("test");
     }
 
 }
+
