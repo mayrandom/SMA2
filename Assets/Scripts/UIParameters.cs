0382034d044dd0c530c0afa92f228ba0ee2f72ed
lyfauv <lylfauvel@ensc.fr>
2020-01-15 17:55:56 +0100

    Update UIParameters.cs

--- Assets/Scripts/UIParameters.cs
+++ Assets/Scripts/UIParameters.cs
@@ -1,30 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIParameters : MonoBehaviour
 {
     GameObject timeText;
     string convertedTime;
+    private float minutes;
+    private float seconds;
+    private float timeNow;
 
     // Start is called before the first frame update
     void Start()
     {
         timeText = GameObject.Find("TimeText");
-        if (timeText == null)
-            Debug.Log("caca");
+        Time.timeScale = 1;
+        timeNow = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float timeNow = Time.realtimeSinceStartup;
-        float minutes = Mathf.Round(timeNow / 60);
-        float seconds = Mathf.Round(timeNow % 60);
-        if (timeText == null)
-            Debug.Log("caca");
+        timeNow = Mathf.Round(Time.timeSinceLevelLoad);
+        minutes = Mathf.Floor(timeNow / 60);
+        seconds = Mathf.Round(timeNow % 60);
+
+        if (seconds == 60)
+        {
+            seconds = 0;
+            minutes++;
+        }
         timeText.GetComponent<Text>().text = "Temps écoulé \n" + minutes + " : " + seconds;
 
+        if (minutes >= 5)
+            SceneManager.LoadScene("EndScene");
     }
 }
